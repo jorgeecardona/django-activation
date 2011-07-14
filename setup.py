f2693c89cbe0ff41fa8ddda107f619b4dadf4799
Jorge Eduardo Cardona <jorgeecardona@gmail.com>
2011-07-14 07:42:09 -0500

    Add license

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup, find_packages
 from os.path import join, abspath, dirname
-from random import choice
-from string import lowercase
 
 
 def read_file(name):
@@ -14,10 +12,11 @@ setup(
     name='django-activation',
     description='Simple activation of users.',
     long_description=read_file('README.rst'),
-    version='0.1.2%s' % (choice(lowercase), ),
+    version='0.1.2',
     author='Jorge Eduardo Cardona Gaviria',
     author_email='jorgeecardona@gmail.com',
     packages=find_packages(),
+    license='BSD',
     install_requires=[
         'django',
         ])
